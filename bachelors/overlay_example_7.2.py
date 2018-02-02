a160d7395fb5ee180b40696cf74caaedb211b7fb
Reuben Lindroos <rllindroos@gmail.com>
2018-02-02 12:36:37 +0100

    for example_7.2_tapered_filter.png

--- bachelors/overlay_example_7.2.py
+++ bachelors/overlay_example_7.2.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 from myplot import myplot
 
 pi=np.pi
-numSamples=128
+numSamples=24
 dt=1./numSamples
 
 xx=np.linspace(-11./24,12./24,numSamples)
@@ -17,11 +17,19 @@ def standard_square_pulse(n,width,c=1.,tweek=0):
 	filt[n/2-(width/2+tweek+1)],filt[n/2+(width/2+tweek+1)]=c/2,c/2
 	filt[n/2-(width/2+tweek):n/2+(width/2+1+tweek)]=c
 	return filt
-#hn=np.fft.fftshift([0,0,0,0,0,0,0,0,0,0,1./8,1./4,1./4,1./4,1./8,0,0,0,0,0,0,0,0,0])
+
 hn=np.fft.fftshift(standard_square_pulse(numSamples,int(0.125*numSamples),c=1./(numSamples/6)))
 
-fx2=np.fft.ifft(np.fft.fft(fx)*np.fft.fft(hn))
+def tapering_filt(n,dx,width,c=1.,tweek=0):
+	filt=np.zeros(n)
+	filt[n/2-(width/2+tweek):n/2+(width/2+1+tweek)]=c
+	for i in range(dx):
+		filt[n/2-(width/2+tweek+i)],filt[n/2+(width/2+tweek+i)]=(dx-i)*(c/dx),(dx-i)*(c/dx)
+	return filt
 
+#hn=np.fft.fftshift(tapering_filt(numSamples,numSamples/4,int(0.125*numSamples),c=1./6))
+fx2=np.fft.ifft(np.fft.fft(fx)*np.fft.fft(hn))
+#fx2=np.fft.ifft(np.fft.fft(fx)*hn)
 def fft_plot_and_sublpot(xx,fx,dt=1):
 	n=len(xx)
 	
@@ -34,13 +42,24 @@ def fft_plot_and_sublpot(xx,fx,dt=1):
 	plt.legend()
 	plt.xlabel('X')
 
-#fft_plot_and_sublpot(xxp,np.fft.fft(hn).real)
-#plt.show()
+def ifft_plot_and_sublpot(xx,fx,dt=1):
+	n=len(xx)
+	
+	plt.subplot(211)
+	plt.plot(xx,np.fft.ifft(fx), label="time domain")
+	plt.legend()
+	
+	plt.subplot(212)
+	myplot(len(xx)*np.fft.fftfreq(len(xx)),fx,label="frequency domain")
+	plt.legend()
+	plt.xlabel('X')
+fft_plot_and_sublpot(xx,hn)
 
-#plt.figure()
+plt.figure()
 
 
 plt.subplot(211)
+plt.title('Square pulse low pass filter')
 plt.plot(xx,fx,label='unthresholded function f(x)')
 plt.legend()
 
@@ -51,11 +70,6 @@ plt.xlabel('X')
 plt.legend()
 
 print(np.linalg.norm(abs(fx1-fx2),2))
-"""
-plt.figure()
-plt.plot(xx,abs(fx1-fx2))
-plt.xlabel('X')
-plt.title('absolute difference')
-"""
+
 plt.show()
 
